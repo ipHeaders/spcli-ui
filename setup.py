4bc803edd7e0bcf0869794528893a57d8dda30a2
Lazaro Mas <mas.lazaro@yahoo.com>
2022-09-20 17:24:26 -0400

    adding pything setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import pathlib
+#from distutils.core import setup
+
+from setuptools import find_packages,setup
+
+here = pathlib.Path(__file__).parent.resolve()
+
+install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
+
+setup(
+    name="pyspcli-ui",
+    version='0.1.0',
+    py_modules=['sp-ui'],
+    setup_requires=["setuptools"],
+    install_requires=install_requires,
+    packages=find_packages(where="spcli-ui"),
+    package_dir={"": "spcli-ui"},
+    include_package_data=True,
+    package_data={"": ["*.py",]},
+
+    #zip_safe=False,
+    entry_points='''
+        [console_scripts]
+        spcli-ui=spcli-ui:main
+    ''',
+    python_requires=">=3.7, <4",
+)

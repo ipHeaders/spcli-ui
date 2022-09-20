413a7d2996ba9a055050a659c800c49791a264c7
Lazaro Mas <mas.lazaro@yahoo.com>
2022-09-20 17:50:47 -0400

    test

--- setup.py
+++ setup.py
@@ -10,18 +10,18 @@ install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').split
 setup(
     name="pyspcli-ui",
     version='0.1.0',
-    py_modules=['sp-ui'],
+    py_modules=['spui'],
     setup_requires=["setuptools"],
     install_requires=install_requires,
-    packages=find_packages(where="spcli-ui"),
-    package_dir={"": "spcli-ui"},
+    packages=find_packages(where="spcli_ui"),
+    package_dir={"": "spcli_ui"},
     include_package_data=True,
     package_data={"": ["*.py",]},
 
     #zip_safe=False,
     entry_points='''
         [console_scripts]
-        cli-ui=cli-ui:main
+        cli_ui=cli_ui:main
     ''',
     python_requires=">=3.7, <4",
 )

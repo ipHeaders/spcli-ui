70567b78cb7aa90e6aef11c5b67af521e5d8c2c2
Lazaro Mas <mas.lazaro@yahoo.com>
2022-09-20 20:13:07 -0400

    testing

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     #zip_safe=False,
     entry_points='''
         [console_scripts]
-        spui=spui:main
+        spcli_ui/spui=spcli_ui/spui:main
     ''',
     python_requires=">=3.7, <4",
 )
